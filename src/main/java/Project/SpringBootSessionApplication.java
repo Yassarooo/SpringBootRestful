9e8f6005a0919a6e83d0fb3769136d628de9a46d
Yassar Hammami <yassarhammami@gmail.com>
2021-01-13 05:14:07 +0200

    .

--- src/main/java/Project/SpringBootSessionApplication.java
+++ src/main/java/Project/SpringBootSessionApplication.java
@@ -6,17 +6,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EntityScan(basePackages = {"Project.domain" })
+@EnableJpaRepositories(basePackages = {"Project.repository"})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableTransactionManagement
 @EnableCaching

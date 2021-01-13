e3a69ccbaf141a6e7f5c607afb57bc33b13f7a99
Yassar Hammami <yassarhammami@gmail.com>
2021-01-13 05:10:48 +0200

    .

--- src/main/java/Project/SpringBootSessionApplication.java
+++ src/main/java/Project/SpringBootSessionApplication.java
@@ -4,13 +4,19 @@ import Project.service.ParamsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableTransactionManagement
 @EnableCaching

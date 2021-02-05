348db2f630a65f616d232444bed339f243b1d915
Yassar Hammami <yassarhammami@gmail.com>
2021-02-05 21:46:39 +0200

    OrderBy Upload Controller

--- src/main/java/Project/web/UploadController.java
+++ src/main/java/Project/web/UploadController.java
@@ -6,10 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,7 +20,7 @@ public class UploadController {
 
 
     @PostMapping("/upload")
-    public ResponseEntity<String> singleFileUpload(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> singleFileUpload(@RequestBody MultipartFile file) {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 

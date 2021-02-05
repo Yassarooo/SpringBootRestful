2418f4fcadb150a63b3abf42348a19a15b59dcea
Yassar Hammami <yassarhammami@gmail.com>
2021-02-05 22:43:35 +0200

    OrderBy Upload Controller

--- src/main/java/Project/web/UploadController.java
+++ src/main/java/Project/web/UploadController.java
@@ -6,9 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,13 +24,17 @@ public class UploadController {
 
 
     @PostMapping("/upload")
-    public ResponseEntity<String> singleFileUpload(@RequestBody MultipartFile file) {
+    public ResponseEntity<String> singleFileUpload(@RequestParam("file") MultipartFile file) {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         //Save the uploaded file to this folder
         String UPLOADED_FOLDER = "E://" + auth.getName() + "//";
 
+        File directory = new File(UPLOADED_FOLDER);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
         if (file.isEmpty()) {
             return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         }
@@ -38,7 +46,7 @@ public class UploadController {
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
 
-            return new ResponseEntity<String>(path.toString(),HttpStatus.OK);
+            return new ResponseEntity<String>(path.toString(), HttpStatus.OK);
 
         } catch (IOException e) {
             e.printStackTrace();

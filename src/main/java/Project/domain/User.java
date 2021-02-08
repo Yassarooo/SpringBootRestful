30805d97fa6c166fbb139413e32cd1181d438943
Yassar Hammami <yassarhammami@gmail.com>
2021-02-08 23:40:15 +0200

    fix User Roles

--- src/main/java/Project/domain/User.java
+++ src/main/java/Project/domain/User.java
@@ -20,7 +20,7 @@ import java.util.*;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @Column(unique = true)

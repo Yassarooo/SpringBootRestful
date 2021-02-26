12eb4c36f034dfe1e50ed3806c7d2d12ebba9813
Yassar Hammami <yassarhammami@gmail.com>
2021-02-26 02:50:05 +0200

    token expire edit

--- src/main/java/Project/config/JwtTokenUtil.java
+++ src/main/java/Project/config/JwtTokenUtil.java
@@ -20,7 +20,7 @@ public class JwtTokenUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
 
-    public static final double JWT_TOKEN_VALIDITY = 0.02 * 60 * 60;
+    public static final double JWT_TOKEN_VALIDITY = 72 * 60 * 60;
 
     @Value("${jwt.secret}")
     private String secret;

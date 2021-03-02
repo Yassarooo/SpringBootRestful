c7c22d97806858d8330ce84ca70ecb0f58453039
Yassar Hammami <yassarhammami@gmail.com>
2021-03-02 03:16:33 +0200

    email verification added.

--- src/main/resources/static/app/controllers/registrationConfirm.js
+++ src/main/resources/static/app/controllers/registrationConfirm.js
@@ -2,4 +2,23 @@ angular.module('NAProject')
 // Creating the Angular Controller
     .controller('registrationConfirmController', function ($http, $scope) {
         $scope.message = "Registration Confirmed successfully ! ";
+
+        var parseQueryString = function () {
+
+            var str = window.location.search;
+            var objURL = {};
+
+            str.replace(
+                new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+                function ($0, $1, $2, $3) {
+                    objURL[$1] = $3;
+                }
+            );
+            return objURL;
+        };
+
+//Example how to use it:
+        var params = parseQueryString();
+        alert(params["token"]);
+
     });

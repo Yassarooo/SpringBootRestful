98bce83a39ee051d95090114f5b73e00afabc89b
Yassar Hammami <yassarhammami@gmail.com>
2021-03-07 01:42:42 +0200

    email verification added.

--- src/main/java/Project/web/RegistrationRestController.java
+++ src/main/java/Project/web/RegistrationRestController.java
@@ -85,7 +85,7 @@ public class RegistrationRestController {
 
     private SimpleMailMessage constructResendVerificationTokenEmail(final String contextPath, final Locale locale, final VerificationToken newToken, final String emailaddress) {
         final String confirmationUrl = contextPath + "/registrationConfirm.html?token=" + newToken.getToken();
-        final String message = messages.getMessage("message.resendToken", null, locale);
+        final String message = messages.getMessage("message.regSuccLink", null, locale);
         return constructEmail("Resend Registration Token", message + " \r\n" + confirmationUrl, emailaddress);
     }
 

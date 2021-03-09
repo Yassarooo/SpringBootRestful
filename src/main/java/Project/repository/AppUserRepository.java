d2926c0e473e8f004a3cb4949124139d2a80d684
Yassar Hammami <yassarhammami@gmail.com>
2021-03-10 00:00:11 +0200

    fixed roles

--- src/main/java/Project/repository/AppUserRepository.java
+++ src/main/java/Project/repository/AppUserRepository.java
@@ -4,8 +4,10 @@ package Project.repository;
 import Project.domain.AppUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
     AppUser findByUsername(String username);
     AppUser findByEmail(String email);
--- src/main/java/Project/repository/CarRepository.java
+++ src/main/java/Project/repository/CarRepository.java
@@ -3,8 +3,10 @@ package Project.repository;
 import Project.domain.Car;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface CarRepository extends CrudRepository<Car, Long> {
 
     Car findAllByparamid(Long paramid);

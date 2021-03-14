033200cb5dd3819e103f1f920ba03bf9f1759505
Yassar Hammami <yassarhammami@gmail.com>
2021-03-14 05:39:00 +0200

    Added Social Auth

--- src/main/java/Project/facebook/FacebookClient.java
+++ src/main/java/Project/facebook/FacebookClient.java
@@ -14,7 +14,7 @@ public class FacebookClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    private final String FACEBOOK_GRAPH_API_BASE = "https://graph.account.com";
+    private final String FACEBOOK_GRAPH_API_BASE = "https://graph.facebook.com";
 
     public FacebookUser getUser(String accessToken) {
         String path = "/me?fields={fields}&redirect={redirect}&access_token={access_token}";
--- src/main/java/Project/google/GoogleService.java
+++ src/main/java/Project/google/GoogleService.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
